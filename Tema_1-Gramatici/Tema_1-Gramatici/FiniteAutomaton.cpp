c7dbed3bf92c5c71f00d5fdadd032e3db7cd57a5
Livia-unitbv <livia.toma@student.unitbv.ro>
2023-11-15 10:54:28 +0200

    added definitions in FiniteAutomanton.cpp

--- Tema_1-Gramatici/Tema_1-Gramatici/FiniteAutomaton.cpp
+++ Tema_1-Gramatici/Tema_1-Gramatici/FiniteAutomaton.cpp
@@ -1,1 +1,20 @@
 #include "FiniteAutomaton.h"
+
+bool FiniteAutomaton::VerifyAutomaton()
+{
+    return false;
+}
+
+void FiniteAutomaton::PrintAutomaton()
+{
+}
+
+bool FiniteAutomaton::CheckWord()
+{
+    return false;
+}
+
+bool FiniteAutomaton::IsDeterministic()
+{
+    return false;
+}
